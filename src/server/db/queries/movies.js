d8400d752da2fe6d1be1065c5a95f66146ed611a
Yan Zaicev <danteivsinero@mail.ru>
2018-07-07 11:59:04 +0300

    Movie routes added

--- src/server/db/queries/movies.js
+++ src/server/db/queries/movies.js
@@ -4,6 +4,36 @@ function getAllMovies() {
   return knex('movies').select('*');
 }
 
+function getSingleMovie(id) {
+  return knex('movies')
+    .select('*')
+    .where({ id: parseInt(id, 10) });
+}
+
+function addMovie(movie) {
+  return knex('movies')
+    .insert(movie)
+    .returning('*');
+}
+
+function updateMovie(id, movie) {
+  return knex('movies')
+    .update(movie)
+    .where({ id: parseInt(id, 10) })
+    .returning('*');
+}
+
+function deleteMovie(id) {
+  return knex('movies')
+    .del()
+    .where({ id: parseInt(id, 10) })
+    .returning('*');
+}
+
 module.exports = {
-  getAllMovies
+  getAllMovies,
+  getSingleMovie,
+  addMovie,
+  updateMovie,
+  deleteMovie
 };
--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,15 @@
 const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
+
 const indexRoutes = require('./routes/index');
+const movieRoutes = require('./routes/movies');
 
 const app = new Koa();
 const PORT = 1337;
 
+app.use(bodyParser);
 app.use(indexRoutes);
+app.use(movieRoutes);
 
 const server = app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
